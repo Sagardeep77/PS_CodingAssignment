fc46e9c516d424db1bcf94b704b23e1cf8cb1bb0
Sagardeep77 <sagardeepkus@gmail.com>
2020-10-27 13:26:48 +0530

    adding preloadingStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
+import { PreloadAllModules } from '@angular/router';
 const routes: Routes = [
   {path : "", component:LandingPageComponent},
   {path : "**", component:LandingPageComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
